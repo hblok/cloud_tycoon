fed8908c8630c2bc12fbdabb4d1661e6280cf415
hblok <github@hblok.net>
2024-10-23 23:32:53 +0200

    proxy, docker

---
+++ docker/local/start_stack.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+bazel run //docker:load_envoy_proxy_native_registry
+bazel run //docker:load_nginx_web_native_registry
+bazel run //docker:load_travel_server_native_registry
+
+docker compose up
