48fe7911db009ca84c7ed16c0858a3ce2ed3c2a7
hblok <github@hblok.net>
2024-11-17 20:32:23 +0100

    Started GCP API test - dependency issue

---
+++ cloud/gcp/gcp_api_test.py
@@ -0,0 +1,15 @@
+
+import unittest
+
+import grpc
+#from grpc._cython import cygrpc
+#from google.cloud import compute_v1
+
+class GcpApiTest(unittest.TestCase):
+
+    def test_list_instances(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
