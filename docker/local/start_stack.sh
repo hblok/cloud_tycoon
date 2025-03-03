7deb13997e03373e137129120fe08adaa1ea6ce4
hblok <github@hblok.net>
2025-03-03 08:54:49 +0100

    Add Alloy

--- docker/local/start_stack.sh
+++ docker/local/start_stack.sh
@@ -4,4 +4,6 @@ bazel run //docker:load_envoy_proxy_native_registry
 bazel run //docker:load_nginx_web_native_registry
 bazel run //docker:load_travel_server_native_registry
 
-docker compose up
+#docker compose up
+docker-compose up -d
+
